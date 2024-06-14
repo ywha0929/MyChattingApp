1a3640b98d3f748eb05770d53393d76826190269
ywha0929 <ywha0929@gmail.com>
2024-06-14 19:45:04 +0900

    0614

--- app/src/main/java/com/example/mychattingapp/MainActivity.java
+++ app/src/main/java/com/example/mychattingapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.mychattingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnHost;
+    Button btnJoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHost = findViewById(R.id.btnHost);
+        btnJoin = findViewById(R.id.btnJoin);
+        btnHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(), HostActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ConnectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
